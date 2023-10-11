05252ffc23870098986a7771824fee86cd06ad73
mehmetozkn <mehmet.trozkan@gmail.com>
2023-10-11 14:05:52 +0300

    repo refactor

--- NewsApp/View/NewsCell.swift
+++ NewsApp/View/NewsCell.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-
 class NewsCell: UITableViewCell {
 
    
@@ -15,13 +14,10 @@ class NewsCell: UITableViewCell {
     @IBOutlet weak var storyLabel: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
 
 }
